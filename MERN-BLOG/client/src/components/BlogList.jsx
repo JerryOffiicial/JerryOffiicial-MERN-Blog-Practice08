c58c03daa257fcedaf895a65b924485c63dd0798
JerryOffiicial <jerrysonjerry1234@email.com>
2026-01-12 13:03:08 +0530

    first commit

---
+++ MERN-BLOG/client/src/components/BlogList.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { blogCategories } from "../assets/assets";
+import { useState } from "react";
+import {motion} from 'motion/react'
+const BlogList = () => {
+  const [menu, setMenu] = useState("All");
+
+  return (
+    <div>
+      <div className="flex justify-center gap-4 sm:gap-8 my-10 relative">
+        {blogCategories.map((item) => (
+          <div key={item} className="relative">
+            <button
+              onClick={() => setMenu(item)}
+              className={`cursor-pointer text-gray-500 ${
+                menu === item && "text-white px-4 pt-0.5"
+              }`}
+            >
+              {item}
+              {menu === item && (
+                <motion.div  layoutId="underline"
+                transition={{type: 'spring', stiffness: 500, damping: 30}}
+                className="absolute inset-0 h-7 -z-1 bg-primary rounded-full"></motion.div>
+              )}
+            </button>
+          </div>
+        ))}
+      </div>
+      <div>{/* --- blog cards */}</div>
+    </div>
+  );
+};
+
+export default BlogList;
