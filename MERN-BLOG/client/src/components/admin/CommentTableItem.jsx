2c14054b62f8e9059c5294bd8579c3aa2fb2107b
JerryOffiicial <jerrysonjerry1234@email.com>
2026-01-15 23:34:03 +0530

    Successfully connected the frontend wit the backend, except the ai integration

--- MERN-BLOG/client/src/components/admin/CommentTableItem.jsx
+++ MERN-BLOG/client/src/components/admin/CommentTableItem.jsx
@@ -1,9 +1,43 @@
 import React from "react";
 import { assets } from "../../assets/assets";
+import { useAppContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
 
 const CommentTableItem = ({ comment, fetchComments }) => {
   const { blog, createdAt, _id } = comment;
   const BlogDate = new Date(createdAt);
+
+  const { axios } = useAppContext();
+
+  const approveComment = async () => {
+    try {
+      const { data } = await axios.post("/api/admin/approve-comment", {id: _id,});
+      if (data.success) {
+        toast.success(data.message);
+        fetchComments();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.success(error.message);
+    }
+  };
+
+  const deleteComment = async () => {
+    try {
+      const confirm = window.confirm('are you sure you want to delete this comment?')
+      if(!confirm) return;
+      const { data } = await axios.post("/api/admin/delete-comment", {id: _id,});
+      if (data.success) {
+        toast.success(data.message);
+        fetchComments();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.success(error.message);
+    }
+  };
 
   return (
     <tr className="order-y border-gray-300">
@@ -24,6 +58,7 @@ const CommentTableItem = ({ comment, fetchComments }) => {
         <div className="inline-flex items-center gap-4">
           {!comment.isApproved ? (
             <img
+              onClick={approveComment}
               src={assets.tick_icon}
               className="w-5 hover:scale-110 transition-all cursor-pointer"
             />
@@ -34,6 +69,7 @@ const CommentTableItem = ({ comment, fetchComments }) => {
           )}
 
           <img
+          onClick={deleteComment}
             src={assets.bin_icon}
             alt=""
             className="w-5 hover:scale-110 transition-all cursor-pointer"
